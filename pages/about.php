e92d906198c08368b268b7bc0be8c2f77efa6746
5h1nN <myassignmentmail@gmail.com>
2016-10-12 02:09:03 +0800

    Add files via upload
    first draft - no design, no information, gt freaking nothing except our names and some copied logo

---
+++ pages/about.php
@@ -0,0 +1,27 @@
+<?php include 'header.php'; ?>
+<!-- Begin page content -->
+<div class="container">
+    <h1<Our Site</h1>
+	
+	<h2>Team Members</h2>
+	<ul>
+		<li>Wen Jie (Project Leader)</li>
+		<li>Chua Wei Kiat</li>
+		<li>Jing Yi</li>
+		<li>Wan Lin</li>
+		<li>Xing Zhao</li>
+		<li>Yaran</li>
+
+	</ul>
+	
+	<h2>Affiliation</h2>
+	<table width="100%">
+	<tr>
+	<td><img src="../img/moe-logo.png" alt="MOE SG logo" /></td>
+	<td><img src="../img/SCSE_logo.jpg" alt="NTU SCSE logo" /></td>
+	</tr>
+	</table>
+
+	
+</div>
+<?php include 'footer.php'; ?>
