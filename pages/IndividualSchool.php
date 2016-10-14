13ad06abd62823fcdba8b26123061db619f27522
5h1nN <myassignmentmail@gmail.com>
2016-10-14 10:48:57 +0800

    change the path to the school logo directory

--- pages/IndividualSchool.php
+++ pages/IndividualSchool.php
@@ -12,13 +12,17 @@ function submitform(){
 $_SESSION["school_name"] = $_GET["school_name"];
 $name = $_SESSION["school_name"];
 $results = searchSchool($name);
+$pic = str_replace(" ", "-",$name);
 ?>
 
 <div class="container">
-<table style="100%" border="1px">
+<table style="100%" border="0">
 	<tr>
 		<td rowspan="9" width="20%">
-			<img src="../img/school_icon/<?php echo $name?>.png" alt="image of the school"/>
+			<img src="../img/School_Logo/<?php echo $pic?>.jpg" alt="image of the school" width="200" height="200"/>
+		</td>
+		<td rowspan="9">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		</td>
 		<td>
 		    Name: <?php foreach ($results as $result){echo $result['school_name'];}?> <form id="form1" action="route.php" method="POST"><input type="hidden" name="name" value="<?php echo $name ?>" ?><a href="javascript: submitform()">How to get there ?</a></form>
@@ -32,13 +36,13 @@ $results = searchSchool($name);
 	<tr><td>Website: <?php foreach ($results as $result){echo $result['school_website'];}?></td></tr>
 	<tr><td>Nearest MRT: <?php foreach ($results as $result){echo $result['MRT'];}?></td></tr>
 	<tr><td>Nearest Bus: <?php foreach ($results as $result){echo $result['Bus'];}?></td></tr>
-	<tr><td colspan="2">CCA: 
+	<tr><td colspan="3">CCA: 
 		<ul>
 		<? php echo $result['CCA']; ?>
 		<?php foreach ($results as $result){$str = explode(",",$result['CCA']); foreach ($str as $item) echo "<li>".$item."</li>";} ?>
 		</ul>
 	</td></tr>
-	<tr><td colspan="2">Subjects offered: 
+	<tr><td colspan="3">Subjects offered: 
 			<ul>
 		<? php echo $result['CCA']; ?>
 		<?php foreach ($results as $result){$str = explode(",",$result['school_subject']); foreach ($str as $item) echo "<li>".$item."</li>";} ?>
