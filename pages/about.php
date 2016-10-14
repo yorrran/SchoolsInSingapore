6467b102d513bfccdd3dd726d5375f12ad657af2
5h1nN <myassignmentmail@gmail.com>
2016-10-14 11:09:53 +0800

    include more information

--- pages/about.php
+++ pages/about.php
@@ -1,8 +1,12 @@
 <?php include 'header.php'; ?>
 <!-- Begin page content -->
 <div class="container">
-    <h1<Our Site</h1>
-	
+    <h1>About this Project</h1>
+	<h2>Background</h2>
+	<div>Due to the large diversity of schools, students often faced the problem of choosing the most suitable school for them. As a fellow students, we know that there are alot of factors to consider such as location, CCA, subjects/module offered, achievement etc, to consider when choosing the right one. </div>
+	<h2>Our Site</h2>
+	<div>This web application is designed to help people to efficiently plan and choose their choice of school. Our site offers a wide list of filters that can be used to search the most suitable school for the user. We also include a navigation system which allows the use to search and view the surrounding and the route to the school. User can also add the school to the comparison list via the add to comparison list button or favourite the school via the favourite button.</div>
+	<h1>Core team</h1>
 	<h2>Team Members</h2>
 	<ul>
 		<li>Wen Jie (Project Leader)</li>
