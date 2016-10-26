f094c2201eae4b056a5f5df6b1279d30091fea41
5h1nN <myassignmentmail@gmail.com>
2016-10-26 12:27:01 +0800

    fixed the duplicate record bug

--- pages/addToFav.php
+++ pages/addToFav.php
@@ -7,13 +7,16 @@ if(isset($_POST['favorite']))
 {
 	// echo "testing";
 	$school_name = $_POST['favorite'];
-	add_to_fav_list('user1', $school_name);
+	if(!in_array($_POST['compare'],$_SESSION['clist'])){
+		add_to_fav_list('user1', $school_name);
+	}else{
+		echo 'Duplicate exist';
+	}
 }
 else if(isset($_POST['unfavorite']))
 {
 	$school_name = $_POST['unfavorite'];
 	remove_from_fav_list('user1', $school_name);
 }
-//echo '<javascript>alert("'.$school_name.'");</javascript>'; //not working if fixed pls amend on addToCompare.php too
 //header("location:javascript://history.go(-1)");
 ?>
