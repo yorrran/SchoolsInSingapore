44f9b7c02bd08166204d69bfe1b8705fbe79cba1
unknownxx <xingzhao0321@gmail.com>
2016-10-27 13:50:29 +0800

    Remove unwanted lines.

--- pages/addToFav.php
+++ pages/addToFav.php
@@ -2,21 +2,17 @@
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 include('../backend/searchManager.php');
 
-//var_dump($_POST);
 if(isset($_POST['favorite']))
 {
-	// echo "testing";
 	$school_name = $_POST['favorite'];
-	if(!in_array($_POST['compare'],$_SESSION['clist'])){
+	if(!in_array($_POST['compare'],$_SESSION['clist']))
 		add_to_fav_list('user1', $school_name);
-	}else{
+	else
 		echo 'Duplicate exist';
-	}
 }
 else if(isset($_POST['unfavorite']))
 {
 	$school_name = $_POST['unfavorite'];
 	remove_from_fav_list('user1', $school_name);
 }
-//header("location:javascript://history.go(-1)");
 ?>
